279a6cd6c0af854739f1d90cfceceda3ed17e020
Tony <tony@pal-nat186-167-249.itap.purdue.edu>
2026-02-03 17:00:53 -0500

    Added some macros in alcos.cpp

---
+++ algos.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+#include <iostream>
+#define BIG_P (1000000007)
+using namespace std;
+ 
+using ll = long long;
+using ld = long double;
+#define MAX(a, b) ((a) > (b) ? (a) : (b))
+#define MIN(a, b) ((a) < (b) ? (a) : (b))
+
+ll binpow(ll x, ll y) {
+    ll z = 1;
+    while (y) {
+        if (y & 1) {
+            z = z * x % BIG_P;
+        }
+        x = x * x % BIG_P;
+        y >>= 1;
+    }
+    return z;
+}
+
+ll gcd(ll x, ll y) {
+    while(x > 0 && y > 0) {
+        x = x%y;
+        //cout << x << " " << y << "\n";
+        if(x == 0) {
+            break;
+        }
+        y = y%x;
+        //cout << x << " " << y << "\n";
+    }
+    if(x == 0) {
+        return y;
+    }
+    return x;
+}
--- code.cpp
+++ code.cpp
@@ -1,12 +1,49 @@
 #include <bits/stdc++.h>
+#include <stack>
+#include <set>
+#include <regex>
+#include <map>
 #include <iostream>
-#define BIG_P (1000000007)
+#include <unordered_set>
+#pragma 3
+#define BIG_P (ll)(1000000007)
 using namespace std;
  
 using ll = long long;
 using ld = long double;
-#define MAX(a, b) ((a) > (b) ? (a) : (b))
-#define MIN(a, b) ((a) < (b) ? (a) : (b))
+#define abs(a) ((a) > 0 ? (a) : (-(a)))
+#define max(a, b) ((a) > (b) ? (a) : (b))
+#define min(a, b) ((a) < (b) ? (a) : (b))
+ 
+typedef vector<ll> vll;
+
+ll binpow(ll x, ll y) {
+    ll z = 1;
+    while (y) {
+        if (y & 1) {
+            z = z * x % BIG_P;
+        }
+        x = x * x % BIG_P;
+        y >>= 1;
+    }
+    return z;
+}
+
+ll gcd(ll x, ll y) {
+    while(x > 0 && y > 0) {
+        x = x%y;
+        //cout << x << " " << y << "\n";
+        if(x == 0) {
+            break;
+        }
+        y = y%x;
+        //cout << x << " " << y << "\n";
+    }
+    if(x == 0) {
+        return y;
+    }
+    return x;
+}
 
 int main() {
     std::ios::sync_with_stdio(false);
@@ -14,6 +51,6 @@ int main() {
     cin.exceptions(cin.failbit);
     int t; cin >> t;
     while(t--) {
-        //Code here
+        
     }
 }
